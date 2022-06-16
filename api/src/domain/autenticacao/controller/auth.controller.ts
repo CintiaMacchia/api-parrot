308664a7fb2c5e187ca3f72fee3b59a1ffa60ec4
Pedro Oliveira <pedro_oliva@outlook.com.br>
2022-06-16 16:27:01 -0300

    auths

---
+++ api/src/domain/autenticacao/controller/auth.controller.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const Users = require('../usuario/models');
+const secret = require('../../../config');
+
+export const authController = {
+  async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password)
+        return res
+          .status(400)
+          .json({ message: 'Todas as informções são obrigatórias!' });
+
+      const logUser = await Users.findOne({ where: { email } });
+
+      if (!logUser) {
+        return res
+          .status(401)
+          .json({ message: 'Usuário ou senha inválidos!!' });
+      }
+
+      if (!bcrypt.compareSync(password, logUser.password)) {
+        return res
+          .status(401)
+          .json({ message: 'Usuário ou senha inválidos!!' });
+      }
+
+      const token = jwt.sign(
+        {
+          id: logUser.id,
+          email: logUser.email,
+          admin: logUser.admin,
+        },
+        secret.key
+      );
+
+      res.cookie('autenticacao', token);
+      return res.status(200).json(token);
+    } catch (error) {
+      return res
+        .status(500)
+        .json(
+          'Ocorreu algum erro. Entre em contato com o administrador do sistema'
+        );
+    }
+  },
+};
--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,18 +1,23 @@
-import { UsuarioController } from "../domain/usuario/controllers/usuario.controller";
-import { PostController } from "../domain/post/controller/post.controller";
+import { UsuarioController } from '../domain/usuario/controllers/usuario.controller';
+import { PostController } from '../domain/post/controller/post.controller';
+import { authController } from '../domain/autenticacao/controller/auth.controller';
+import { loginValidation } from '../domain/autenticacao/validation/authLogin';
 
-const express = require("express");
+const auth = require('../middlewares/auth');
+const express = require('express');
 const routes = express.Router();
 
-routes.post("/usuarios", UsuarioController.createUser)
-routes.get("/usuarios", UsuarioController.getAllUsers)
-routes.get("/usuarios/:id", UsuarioController.getOneUser)
-routes.put("/usuarios/:id", UsuarioController.updateUser)
-routes.delete("/usuarios/:id", UsuarioController.deleteUser)
+routes.post('/usuarios', UsuarioController.createUser);
+routes.get('/usuarios', UsuarioController.getAllUsers);
+routes.get('/usuarios/:id', UsuarioController.getOneUser);
+routes.put('/usuarios/:id', UsuarioController.updateUser);
+routes.delete('/usuarios/:id', UsuarioController.deleteUser);
 
-routes.post("/posts", PostController.createPost)
-routes.get("/posts", PostController.getAllPosts)
-routes.get("/posts/:id", PostController.getAllPostsFromOneUser)
-routes.delete("/posts/:id/:user_id", PostController.deletePost)
+routes.post('/posts', PostController.createPost);
+routes.get('/posts', PostController.getAllPosts);
+routes.get('/posts/:id', PostController.getAllPostsFromOneUser);
+routes.delete('/posts/:id/:user_id', PostController.deletePost);
+
+routes.post('/login', loginValidation, auth, authController.login);
 
 module.exports = routes;
